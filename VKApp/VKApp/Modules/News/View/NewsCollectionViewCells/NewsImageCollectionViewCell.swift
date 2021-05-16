5559697eb3dd96e8a86c2871eccdb0c1fba997b1
Igor Potemkin <potemkinigor@yandex.ru>
2021-05-16 13:19:49 +0300

    AppPerfomanceLesson1

---
+++ VKApp/VKApp/Modules/News/View/NewsCollectionViewCells/NewsImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsImageCollectionViewCell.swift
+//  VKApp
+//
+//  Created by User on 16.05.2021.
+//
+
+import UIKit
+
+class NewsImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
